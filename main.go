0af73be56a059b18ba54aee74c17f87603272e05
Luke Lonergan <mynameis2056@yahoo.com>
2024-12-13 16:59:54 -0800

    WIP: Text rendering - currently image is upside down :-)

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ import (
 func main() {
 	chart := chart2d.NewChart2D(-10, 10, -20, 20, 1000, 1000)
 	chart.AddAxis(chart2d.Color{1., 1., 1.})
-	chart.Screen.LoadFont("assets/fonts/Noto-Sans/static/NotoSans-Regular.ttf", 32)
-	chart.Screen.AddString(screen.NEW, "Hello World", -0, 0, [3]float32{1, 1, 1}, 0.5)
+	chart.Screen.LoadFont("assets/fonts/Noto-Sans/static/NotoSans-Regular.ttf", 64)
+	//chart.Screen.LoadFont("assets/fonts/snob.org/sans-serif.fnt", 64)
+	chart.Screen.AddString(screen.NEW, "Hello World - There once was a man in natucket. His head could fill a big bucket. He tried one on, then banged into his lawn. His friends were told to buck it.", 0.5, 0.5, [3]float32{1, 1, 1}, 1.0)
 	select {}
 }
 
--- screen/bitmap_font.go
+++ screen/bitmap_font.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	colorlib "image/color"
 	"image/draw"
+	"image/png"
 	"os"
 	"runtime"
 
@@ -21,38 +22,6 @@ type String struct {
 	Position      mgl32.Vec2
 	Color         [3]float32
 	Texture       uint32
-}
-
-func (line *String) addShader(scr *Screen) (shaderProgram uint32) {
-	if _, present := scr.Shaders[STRING]; !present {
-		vertexShaderSource := `
-		#version 450
-		layout (location = 0) in vec2 position;
-		layout (location = 1) in vec2 uv;
-		layout (location = 2) in vec3 color;
-		uniform mat4 projection;
-		out vec2 fragUV;
-		out vec3 fragColor;
-		void main() {
-			gl_Position = projection * vec4(position, 0.0, 1.0);
-			fragUV = uv;
-			fragColor = color;
-		}` + "\x00"
-
-		fragmentShaderSource := `
-		#version 450
-		in vec2 fragUV;
-		in vec3 fragColor;
-		uniform sampler2D fontTexture;
-		out vec4 outColor;
-		void main() {
-			vec4 texColor = texture(fontTexture, fragUV);
-			outColor = texColor * vec4(fragColor, 1.0);
-		}` + "\x00"
-
-		scr.Shaders[STRING] = compileShaderProgram(vertexShaderSource, fragmentShaderSource)
-	}
-	return scr.Shaders[STRING]
 }
 
 func (scr *Screen) LoadFont(filePath string, fontSize float64) error {
@@ -88,122 +57,203 @@ func (scr *Screen) AddString(key Key, text string, x, y float32, color [3]float3
 	newKey = key
 
 	scr.RenderChannel <- func() {
+		fmt.Println("[AddString] Starting to create text image")
 		var str *String
-		if renderable, exists := scr.Objects[key]; exists {
-			str = renderable.Object.(*String)
+		if object, present := scr.Objects[key]; present {
+			str = object.Object.(*String)
 		} else {
 			str = &String{
-				Text:          text,
-				Position:      mgl32.Vec2{x, y},
-				Color:         color,
-				ShaderProgram: str.addShader(scr),
+				Text:     text,
+				Position: mgl32.Vec2{x, y},
+				Color:    color,
 			}
-		}
-		img := image.NewRGBA(image.Rect(0, 0, 512, 512))
-		draw.Draw(img, img.Bounds(), image.Black, image.Point{}, draw.Src)
-		ctx := freetype.NewContext()
-		ctx.SetDPI(72)
-		ctx.SetFont(scr.Font)
-		ctx.SetFontSize(scr.FontSize * scale)
-		ctx.SetClip(img.Bounds())
-		ctx.SetDst(img)
-		ctx.SetSrc(image.NewUniform(colorlib.RGBA{R: uint8(color[0] * 255), G: uint8(color[1] * 255), B: uint8(color[2] * 255), A: 255}))
-		pt := freetype.Pt(0, int(ctx.PointToFixed(scr.FontSize*scale)>>6))
-		_, err := ctx.DrawString(text, pt)
-		if err != nil {
-			fmt.Printf("Error drawing string: %v\n", err)
-		}
+			str.ShaderProgram = str.addShader(scr)
 
-		var texture uint32
-		gl.GenTextures(1, &texture)
-		gl.BindTexture(gl.TEXTURE_2D, texture)
-		gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 512, 512, 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(img.Pix))
-		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-		gl.BindTexture(gl.TEXTURE_2D, 0)
+			// Create the font image
+			img := image.NewRGBA(image.Rect(0, 0, 512, 512))
+			draw.Draw(img, img.Bounds(), image.Black, image.Point{}, draw.Src)
+			ctx := freetype.NewContext()
+			ctx.SetDPI(72)
+			ctx.SetFont(scr.Font)
+			ctx.SetFontSize(scr.FontSize * scale)
+			ctx.SetClip(img.Bounds())
+			ctx.SetDst(img)
+			ctx.SetSrc(image.NewUniform(colorlib.RGBA{R: uint8(color[0] * 255), G: uint8(color[1] * 255), B: uint8(color[2] * 255), A: 255}))
+			pt := freetype.Pt(0, int(ctx.PointToFixed(scr.FontSize*scale)>>6))
+			_, err := ctx.DrawString(text, pt)
+			if err != nil {
+				fmt.Printf("Error drawing string: %v\n", err)
+			} else {
+				SaveDebugImage(img, "debug_font.png")
+			}
 
-		str.Texture = texture
-		scr.Objects[key] = Renderable{
-			Active: true,
-			Object: str,
+			// Create OpenGL Texture
+			var texture uint32
+			gl.GenTextures(1, &texture)
+			gl.ActiveTexture(gl.TEXTURE0) // Activate texture unit 0
+			gl.BindTexture(gl.TEXTURE_2D, texture)
+			gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 512, 512, 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(img.Pix))
+			gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+			gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+
+			// Calculate proper position and scale
+			width := float32(scr.XMax-scr.XMin) * float32(scale) / 10
+			height := float32(scr.YMax-scr.YMin) * float32(scale) / 10
+			posX := x
+			posY := y
+
+			// Create VAO and VBO for quad
+			vertices := []float32{
+				posX, posY, 0.0, 0.0, // Bottom-left  (position, UV)
+				posX + width, posY, 1.0, 0.0, // Bottom-right (position, UV)
+				posX, posY + height, 0.0, 1.0, // Top-left (position, UV)
+				posX + width, posY + height, 1.0, 1.0, // Top-right (position, UV)
+			}
+
+			gl.GenVertexArrays(1, &str.VAO)
+			gl.GenBuffers(1, &str.VBO)
+			gl.BindVertexArray(str.VAO)
+
+			// Bind VBO and load vertex data
+			gl.BindBuffer(gl.ARRAY_BUFFER, str.VBO)
+			gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
+
+			// Set position attribute (2D position, first 2 floats of each vertex)
+			gl.VertexAttribPointer(0, 2, gl.FLOAT, false, 4*4, gl.PtrOffset(0))
+			gl.EnableVertexAttribArray(0)
+
+			// Set UV attribute (2D UVs, next 2 floats of each vertex)
+			gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 4*4, gl.PtrOffset(2*4))
+			gl.EnableVertexAttribArray(1)
+
+			// Clean up
+			gl.BindBuffer(gl.ARRAY_BUFFER, 0)
+			gl.BindVertexArray(0)
+
+			// Unbind the texture AFTER the VBO/VAO are set up
+			gl.BindTexture(gl.TEXTURE_2D, 0)
+
+			str.Texture = texture
+
+			scr.Objects[key] = Renderable{
+				Active: true,
+				Object: str,
+			}
 		}
 	}
 	return key
 }
 
-func (str *String) Render(scr *Screen) {
-	scr.RenderChannel <- func() {
-		gl.UseProgram(scr.Shaders[STRING])
-		checkGLError("String: After bind texture")
+func (str *String) addShader(scr *Screen) (shaderProgram uint32) {
+	if _, present := scr.Shaders[STRING]; !present {
+		vertexShaderSource := `
+		#version 450
+		layout (location = 0) in vec2 position;
+		layout (location = 1) in vec2 uv;
+		uniform mat4 projection;
+		out vec2 fragUV;
+		void main() {
+			gl_Position = projection * vec4(position, 0.0, 1.0);
+			fragUV = uv;
+		}` + "\x00"
 
-		// Enable blending for transparency
-		gl.Enable(gl.BLEND)
-		gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
-		checkGLError("String: After blend")
+		fragmentShaderSource := `
+		#version 450
+		in vec2 fragUV;
+		uniform sampler2D fontTexture;
+		out vec4 outColor;
+		void main() {
+			vec4 texColor = texture(fontTexture, fragUV);
+			outColor = texColor; // Display texture without multiplying it with another color
+		}` + "\x00"
 
-		// Disable depth testing to ensure text is not obscured
-		gl.Disable(gl.DEPTH_TEST)
-		checkGLError("String: After disable depth text")
-
-		projectionUniform := gl.GetUniformLocation(scr.Shaders[STRING], gl.Str("projection\x00"))
-		if projectionUniform >= 0 {
-			gl.UniformMatrix4fv(projectionUniform, 1, false, &scr.projectionMatrix[0])
-			checkGLError("String: After UniformMatrix4fv")
+		shaderProgram := compileShaderProgram(vertexShaderSource, fragmentShaderSource)
+		if shaderProgram == 0 {
+			panic("Failed to compile shader program for STRING")
 		}
-
-		gl.ActiveTexture(gl.TEXTURE0)
-		textureUniform := gl.GetUniformLocation(scr.Shaders[STRING], gl.Str("fontTexture\x00"))
-		gl.Uniform1i(textureUniform, 0)
-		checkGLError("String: After Uniform1i")
-		gl.BindTexture(gl.TEXTURE_2D, str.Texture)
-		checkGLError("String: After binftexture")
-
-		// Render the quad for the text
-		gl.GenVertexArrays(1, &str.VAO)
-		checkGLError("String: After GenVertexArrays")
-		gl.BindVertexArray(str.VAO)
-		checkGLError("String: After BindVertexArrays")
-
-		// Position and UV coordinates for the text quad
-		vertices := []float32{
-			str.Position[0], str.Position[1], 0.0, 0.0,
-			str.Position[0] + 1.0, str.Position[1], 1.0, 0.0,
-			str.Position[0], str.Position[1] + 1.0, 0.0, 1.0,
-			str.Position[0] + 1.0, str.Position[1] + 1.0, 1.0, 1.0,
-		}
-
-		gl.GenBuffers(1, &str.VBO)
-		gl.BindBuffer(gl.ARRAY_BUFFER, str.VBO)
-		gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
-		checkGLError("String: 1")
-
-		gl.VertexAttribPointer(0, 2, gl.FLOAT, false, 4*4, gl.PtrOffset(0))
-		gl.EnableVertexAttribArray(0)
-		checkGLError("String: 2")
-
-		gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 4*4, gl.PtrOffset(2*4))
-		gl.EnableVertexAttribArray(1)
-		checkGLError("String: 3")
-
-		gl.DrawArrays(gl.TRIANGLE_STRIP, 0, 4)
-		checkGLError("String: DrawArrays")
-
-		// Cleanup
-		gl.DisableVertexAttribArray(0)
-		gl.DisableVertexAttribArray(1)
-		gl.BindBuffer(gl.ARRAY_BUFFER, 0)
-		gl.BindVertexArray(0)
-		gl.DeleteBuffers(1, &str.VBO)
-		gl.DeleteVertexArrays(1, &str.VAO)
-		checkGLError("String: Cleanup")
-
-		// Unbind texture
-		gl.BindTexture(gl.TEXTURE_2D, 0)
-		checkGLError("String: Unbind")
-
-		// Restore OpenGL state
-		gl.Enable(gl.DEPTH_TEST)
-		gl.Disable(gl.BLEND)
-		checkGLError("String: Enable Depth_test, disable blend")
+		fmt.Println("[AddShader] Successfully created shader program for STRING")
+		scr.Shaders[STRING] = shaderProgram
 	}
+	return scr.Shaders[STRING]
+}
+
+func SaveDebugImage(img *image.RGBA, filename string) {
+	if img == nil {
+		fmt.Println("[SaveDebugImage] Image is nil, nothing to save.")
+		return
+	}
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Printf("[SaveDebugImage] Failed to create image file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	err = png.Encode(file, img)
+	if err != nil {
+		fmt.Printf("[SaveDebugImage] Failed to save image as PNG: %v\n", err)
+	} else {
+		fmt.Printf("[SaveDebugImage] Image saved as '%s'.\n", filename)
+	}
+}
+
+func (str *String) Render(scr *Screen) {
+	gl.UseProgram(scr.Shaders[STRING])
+	checkGLError("After UseProgram")
+
+	// Enable depth testing
+	gl.Enable(gl.DEPTH_TEST)
+	checkGLError("After Enable DEPTH_TEST")
+
+	// Check if the active program matches
+	var activeProgram int32
+	gl.GetIntegerv(gl.CURRENT_PROGRAM, &activeProgram)
+	if uint32(activeProgram) != scr.Shaders[STRING] {
+		fmt.Printf("[Render] Shader program mismatch! Active: %d, Expected: %d\n", activeProgram, scr.Shaders[STRING])
+		panic("[Render] Shader program is not active as expected")
+	}
+
+	if scr.Shaders[STRING] == 0 {
+		fmt.Println("[Render] Shader program handle is 0. Possible compilation/linking failure.")
+		panic("[Render] Shader program handle is 0")
+	}
+
+	// Set projection matrix
+	projectionUniform := gl.GetUniformLocation(scr.Shaders[STRING], gl.Str("projection\x00"))
+	checkGLError("After GetUniformLocation")
+	if projectionUniform < 0 {
+		fmt.Println("[Render] Projection uniform not found!")
+		panic("[Render] Projection uniform location returned -1")
+	}
+	gl.UniformMatrix4fv(projectionUniform, 1, false, &scr.projectionMatrix[0])
+	checkGLError("After UniformMatrix4fv")
+
+	// Activate texture unit and bind the texture
+	gl.ActiveTexture(gl.TEXTURE0)
+	checkGLError("After ActiveTexture")
+	gl.BindTexture(gl.TEXTURE_2D, str.Texture)
+	checkGLError("After BindTexture")
+
+	// Bind the shader texture uniform to the correct texture unit
+	textureUniform := gl.GetUniformLocation(scr.Shaders[STRING], gl.Str("fontTexture\x00"))
+	checkGLError("After GetUniformLocation for fontTexture")
+	if textureUniform < 0 {
+		fmt.Println("[Render] fontTexture uniform not found!")
+		panic("[Render] fontTexture uniform location returned -1")
+	}
+	gl.Uniform1i(textureUniform, 0)
+	checkGLError("After Uniform1i")
+
+	// Bind the VAO (Vertex Array Object)
+	gl.BindVertexArray(str.VAO)
+	checkGLError("After BindVertexArray")
+
+	// Draw the quad (two triangles)
+	gl.DrawArrays(gl.TRIANGLE_STRIP, 0, 4)
+	checkGLError("After DrawArrays")
+
+	// Unbind VAO and texture to avoid side effects
+	gl.BindVertexArray(0)
+	gl.BindTexture(gl.TEXTURE_2D, 0)
+	checkGLError("After UnbindVertexArray and UnbindTexture")
 }
